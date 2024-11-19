686a316f93f2304b63ea03808061864f5765d398
jaimemin <jaimemin@gmail.com>
2024-11-19 23:13:32 +0900

    Ch6_CoroutineContext

--- src/main/kotlin/chapter6/Code6-3.kt
+++ src/main/kotlin/chapter6/Code6-3.kt
@@ -1,8 +1,18 @@
 package chapter6
 
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-val coroutineContext1 = CoroutineName("MyCoroutine1") + newSingleThreadContext("MyThread1")
-val coroutineContext2 = CoroutineName("MyCoroutine2") + newSingleThreadContext("MyThread2")
+val sampleJob = Job()
+val coroutineContext1: CoroutineContext =
+    CoroutineName("MyCoroutine1") + newSingleThreadContext("MyThread1") + sampleJob
+val coroutineContext2: CoroutineContext = CoroutineName("MyCoroutine2") + newSingleThreadContext("MyThread2")
 val combinedCoroutineContext = coroutineContext1 + coroutineContext2
+
+fun main() = runBlocking<Unit> {
+    launch(context = combinedCoroutineContext) {
+        println("[${Thread.currentThread().name}] 실행")
+        println(combinedCoroutineContext[Job])
+    }
+    delay(100) // 실행 시간을 확보
+}
