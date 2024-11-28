8c6585b8b4e6100c97d857b9f9d73ebea59a987c
jaimemin <jaimemin@gmail.com>
2024-11-28 23:57:51 +0900

    Ch11_Coroutine Deep Dive

--- src/main/kotlin/chapter11/Code11-3.kt
+++ src/main/kotlin/chapter11/Code11-3.kt
@@ -1,18 +1,24 @@
 package chapter11
 
-//var count = 0
-//val mutex = Mutex()
-//
-//fun main() = runBlocking<Unit> {
-//    withContext(Dispatchers.Default) {
-//        repeat(10_000) {
-//            launch {
-//                mutex.lock()
-//                count += 1
-//                mutex.unlock()
-//            }
-//        }
-//    }
-//
-//    println("count = ${count}")
-//}
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.withContext
+
+var count = 0
+val mutex = Mutex()
+
+fun main() = runBlocking<Unit> {
+    withContext(Dispatchers.Default) {
+        repeat(10_000) {
+            launch {
+                mutex.lock()
+                count += 1
+                mutex.unlock()
+            }
+        }
+    }
+
+    println("count = ${count}")
+}
